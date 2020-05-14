a8b1c56f4ef08a57eeaba67b0a89ad0f281a674b
Richard Hajek <richard.m.hajek@gmail.com>
2020-05-14 20:32:11 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,16 @@
+const secret = require('./secret.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+console.log("Using token: " + secret.token)
+client.login(secret.token);
